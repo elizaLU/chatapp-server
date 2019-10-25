82d3be47fef9aeffa6c80be52976064f7cd07bf8
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-10-25 12:08:29 +0200

    install & use cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
 const streamRouter = require('./stream/router')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000
 //all data sent over the net needs to be in JSON format, so we JSON all data we receive after we request it
 const jsonParser = bodyParser.json()
 app.use(jsonParser)
-
+app.use(cors()) //cors function is executed ()
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
