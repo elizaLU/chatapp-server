394c6b9f1a51a55fedaa4c8cc4233bf045bfc866
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-10-25 11:47:02 +0200

    update the steam between post and get reqs

--- stream/router.js
+++ stream/router.js
@@ -9,8 +9,8 @@ const stream = new Sse()
 router.get('/stream', async (req, res) => {
   console.log("req on /stream")
 
-  // res.status(200) --> remove stteam must handle this connection
-  // res.send('/stream works') --> remove stteam must handle this connection
+  // res.status(200) --> remove stream must handle this connection
+  // res.send('/stream works') --> remove stream must handle this connection
   //here we get data that we want to stream from the database:
   const message = await Chatroom.findAll()
   const data = JSON.stringify(message)
@@ -24,12 +24,17 @@ router.get('/stream', async (req, res) => {
 router.post('/message', async (req, res) => {
   console.log('welcome message', req.body)
   const { message } = req.body
-  //aeait till creation of my message is done..
+  //await till creation of my message is done..
   const entity = await Chatroom.create({
     message: message
   })
-  res.status(201)
-  res.send('message received')
+  //stream posted messages too:
+  const message = await Chatroom.findAll()
+  const data = JSON.stringify(message)
+
+  stream.send(data)
+  // res.status(201)       --> remove stream must handle this connection
+  // res.send('message received')    --> remove stream must handle this connection
 })
 
 
